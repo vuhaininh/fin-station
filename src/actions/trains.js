c8311aba4fdb909d4613ee832287de8f03043088
Hai Ninh Vu <haininh.vu89@gmail.com>
2017-07-20 23:18:29 +0300

    update train

--- src/actions/trains.js
+++ src/actions/trains.js
@@ -8,30 +8,27 @@ export function fetchTrains(state){
 
   // Create Request using axios
   const request = axios.get(requestUrl);
-
+  var trains;
   return (dispatch) => {
     request.then( (response) => {
       // Fetch successfully
-      var trains = response.data; // Fetched Trains
-      Promise.all(trains.map( (train) => {
+      trains = response.data; // Fetched Trains
+      return Promise.all(trains.map( (train) => {
           // Get Composition of each Train
 
           var compositionRequestUrl = `${constants.ROOT_URL}/compositions/${train.trainNumber}?departure_date=${train.departureDate}`; //request URL
           return axios.get(compositionRequestUrl); //axios request
         })
-      ).then((compositions) => {
-          //Update Train Info based on fetched Composition
-          var updatedTrains = trains.map( (train,i) => {
-              train.composition = compositions[i].data;
-              return train;
-          })
-          //Dispatch actions to Reducers to get updated state of Redux Store
-          dispatch({type: constants.FETCH_TRAINS, payload: updatedTrains}); // Update Train List
+      )
 
-        }).catch( (error) => {
-          // catch error  in case cannot fetch Composition Info, dispath to Reducers to update state
-           dispatch({type: constants.ERROR_GENERAL, payload: error}); // Send notification about error
-        });
+    }).then((compositions) => {
+        //Update Train Info based on fetched Composition
+        var updatedTrains = trains.map( (train,i) => {
+            train.composition = compositions[i].data;
+            return train;
+          })
+        //Dispatch actions to Reducers to get updated state of Redux Store
+        dispatch({type: constants.FETCH_TRAINS, payload: updatedTrains}); // Update Train List
 
     }).catch( (error) => {
       // catch error  in case cannot fetch data, dispath to Reducers to update state
